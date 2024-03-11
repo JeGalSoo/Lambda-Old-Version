4cd67555360caf40ce4cddc7feed1acf056aa3c5
JeGalSoo <160007544+JeGalSoo@users.noreply.github.com>
2024-03-11 21:07:25 +0900

    240311 Strategy Pattern

--- lambda-develop-1.1/com/turing/api/Main.java
+++ lambda-develop-1.1/com/turing/api/Main.java
@@ -1,25 +1,19 @@
 package com.turing.api;
 
 
+
+import com.turing.api.enums.Menus;
+
+import javax.swing.text.NavigationFilter;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static final Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws IOException, SQLException {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        Scanner sc = new Scanner(System.in);
 
-        while (true){
-            System.out.println("=== x-Exit\n" +
-                    "u-User" +
-                    "b-Review" +
-                    "m-Account" +
-                    "c-Crawler" +
-                    "===");
+        while (Menus.getOperator(sc)) ;
 //            switch (sc.next()){
 //                case "x":  return;
 //                case "u": UserView.main(sc);break;
@@ -27,7 +21,5 @@ public class Main {
 //                case "c": CrawlerView.main(sc);  break;
 //                case "b": ReViewView.main(sc); break;
 //            }
-        }
-
     }
 }
