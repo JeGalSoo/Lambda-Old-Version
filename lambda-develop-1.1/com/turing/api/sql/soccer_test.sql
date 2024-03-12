d95ac00873340ce3d7eb00ada44da3e2deee0a6b
JeGalSoo <160007544+JeGalSoo@users.noreply.github.com>
2024-03-12 16:23:17 +0900

    240312 sql design pattern

--- lambda-develop-1.1/com/turing/api/sql/soccer_test.sql
+++ lambda-develop-1.1/com/turing/api/sql/soccer_test.sql
@@ -21,4 +21,36 @@ select *
 from player
 where team_id='k02' and player_name like '고%' and height>=170;
 --005-1 수원팀에서 성이 고씨이고 키가 170 이상인 선수를 출력하시오. (ID를 모를 경우)
-select * from team;
+select *
+from player
+where height>=170 and player_name like '고%' and team_id=(select team_id from team where region_name='수원')
+-----------------------------------------------------------
+select *
+from stadium;
+select *
+from schedule;
+select *
+from team;
+select *
+from player;
+-- 문제 6
+-- 다음 조건을 만족하는 선수명단을 출력하시오
+-- 소속팀이 삼성블루윙즈이거나
+-- 드래곤즈에 소속된 선수들이어야 하고,
+-- 포지션이 미드필더(MF:Midfielder)이어야 한다.
+-- 키는 170 센티미터 이상이고 180 이하여야 한다.
+SELECT player_name, height, team_id
+FROM player
+WHERE POSITION='mf' AND height between 170 and 180 AND team_id=(SELECT team_id FROM team WHERE team_name='삼성블루윙즈');
+--수원을 연고지로 하는 골키퍼는 누구인가?
+select player_name
+from player
+where POSITION='GK' and team_id=(select team_id from team where region_name= '수원' );
+-- 문제 8-- 서울팀 선수들 이름, 키, 몸무게 목록으로 출력하시오-- 키와 몸무게가 없으면 "0" 으로 표시하시오-- 키와 몸무게는 내림차순으로 정렬하시오
+select player_name, if(height=0,0,height), if(weight=0,0,weight)
+from player
+order by height desc, weight desc;
+-- 문제 9-- 서울팀 선수들 이름과 포지션과-- 키(cm표시)와 몸무게(kg표시)와  각 선수의 BMI지수를 출력하시오-- 단, 키와 몸무게가 없으면 "0" 표시하시오-- BMI는 "NONE" 으로 표시하시오(as bmi)-- 최종 결과는 이름내림차순으로 정렬하시오
+select player_name, position, if(height='',0,concat(height,'cm')), if(weight='',0,concat(weight,'kg')), round(if(weight=0 or height=0,'NONE',weight/((height/100)*(height/100))),1) as bmi
+from player
+order by player_name desc;
