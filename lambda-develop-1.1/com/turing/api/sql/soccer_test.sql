0bbe630ef4dfe7b6b043ecb3ae559a9510dee9e3
JeGalSoo <160007544+JeGalSoo@users.noreply.github.com>
2024-03-13 12:03:50 +0900

    240313 sql join

--- lambda-develop-1.1/com/turing/api/sql/soccer_test.sql
+++ lambda-develop-1.1/com/turing/api/sql/soccer_test.sql
@@ -58,3 +58,53 @@ select player_name, position, if(height='',0,concat(height,'cm')), if(weight='',
 from player
 order by player_name desc;
 ----------------------------------------------------------------------------
+--full scan
+--4개의 테이블을 모두 조회하시오
+select count(*) count
+from stadium s
+join team t on t.stadium_id = s.stadium_id
+join schedule g on g.stadium_id=s.stadium_id
+join player p on p.team_id=t.team_id
+-- 문제 10
+-- 수원팀(K02) 과 대전팀(K10) 선수들 중 포지션이 골키퍼(GK)인 선수를 출력하시오
+-- 단 , 팀명, 선수명 오름차순 정렬하시오
+select player_name
+from team t
+join player p on t.team_id=p.team_id
+where position='GK' and t.team_id in ('K02','K10')
+order by t.team_id ,p.player_name;
+-- 문제 11
+-- 팀과 연고지를 연결해서 출력하시오
+-- [팀 명]             [홈구장]
+-- 수원[ ]삼성블루윙즈 수원월드컵경기장
+select concat(region_name,'[]',team_name,' ',s.stadium_name)
+from stadium s
+join team t on s.stadium_id=t.stadium_id
+-- 문제 12
+-- 수원팀(K02) 과 대전팀(K10) 선수들 중
+-- 키가 180 이상 183 이하인 선수들
+-- 키, 팀명, 사람명 오름차순
+select player_name
+from team t
+join player p on t.team_id=p.team_id
+where p.height between 180 and 183 and team_id in ('k02','k10')
+order by team_name, player_name;
+-- 문제 13
+-- 모든 선수들 중 포지션을 배정 받지 못한 선수들의
+-- 팀명과 선수이름 출력 둘다 오름차순
+select team_name, player_name
+from team t
+join player p on t.team_id=p.team_id
+where `POSITION`=''
+order by team_name, player_name
+--스칼라와 조인  사용
+ -- 문제 14
+-- 팀과 스타디움, 스케줄을 조인하여
+-- 2012년 3월 17일에 열린 각 경기의
+-- 팀이름, 스타디움, 어웨이팀 이름 출력
+-- 다중테이블 join 을 찾아서 해결하시오.
+select awayteam_id,
+(select stadium_name from stadium where stadium.stadium_id=schedule.stadium_id) as stadium_name,
+(select team_name from team where team.stadium_id=schedule.stadium_id) as team_name
+from schedule
+where sche_date=20120317;
